cc89742349e0239e259c06b17252e8aad23a8e13
shreyk17 <shreyk.11@gmail.com>
2022-04-07 08:07:18 +0530

    details bid page

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,10 +1,93 @@
-import { View, Text } from 'react-native'
+import { View, Text , Image , TextInput } from 'react-native'
 import React from 'react'
+import {COLORS , FONTS , SIZES , SHADOWS , assets} from '../constants'
 
-const HomeHeader = () => {
+const HomeHeader = ({onSearch}) => {
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View style  ={{
+      backgroundColor :COLORS.primary,
+      padding : SIZES.font
+    }}>
+      <View style = {{
+        flexDirection : 'row',
+        justifyContent : 'space-between',
+        alignItems  :'center'
+      }}>
+        <Image  
+          source={assets.logo}
+          resizeMode = "contain"
+          style = {{
+            width : 90,
+            height : 25
+          }}
+        />
+         <View style = {{width : 45 , height : 45}} >
+          <Image source={assets.person01}
+            resizeMode = 'contain'
+            style = {{
+              width : "100%",
+              height : '100%'
+            }}
+          />
+          <Image source={assets.badge}
+            resizeMode = 'contain'
+            style = {{
+              width : 15,
+              height : 15,
+              position : 'absolute',
+              bottom : 0,
+              right : 0
+            }}
+          />
+        </View>
+      </View>
+      <View style = {{
+        marginVertical : SIZES.font
+      }}>
+        <Text style = {{
+          fontSize : SIZES.small,
+          fontFamily : FONTS.regular,
+          color : COLORS.white
+        }}>
+          Hello, EDITH 👋
+        </Text>
+        <Text style = {{
+          fontSize : SIZES.large,
+          fontFamily : FONTS.bold,
+          color : COLORS.white,
+          marginTop : SIZES.base/2
+        }}>
+          Let's find a masterpiece
+        </Text>
+      </View>
+      <View style = {{
+        marginTop : SIZES.font
+      }}>
+        <View style = {{
+          width : '100%',
+          borderRadius : SIZES.font,
+          backgroundColor : COLORS.gray,
+          flexDirection  :'row',
+          alignItems : 'center',
+          paddingHorizontal : SIZES.font,
+          paddingVertical : SIZES.small-2
+        }}>
+          <Image 
+            source={assets.search}
+            resizeMode = 'contain'
+            style = {{
+              width : 20,
+              height : 20,
+              marginRight  :SIZES.base
+            }}
+          />
+          <TextInput 
+            placeholder='Search NFTS'
+            style = {{flex : 1}}
+            onChangeText = {onSearch}
+          />
+        </View>
+      </View>
     </View>
   )
 }
--- components/NftCard.js
+++ components/NftCard.js
@@ -1,7 +1,7 @@
 import { View, Image , Text } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { COLORS , SIZES , SHADOWS , assets } from '../constants'
-import { CircleButton } from './Button'
+import { CircleButton, RectButton } from './Button'
 import { SubInfo , ETHPrice , NFTitle } from './SubInfo'
 
 const NftCard = ({data}) => {
@@ -32,7 +32,23 @@ const NftCard = ({data}) => {
 
         </View>
             
-           <SubInfo /> 
+           <SubInfo />
+
+           <View style ={{
+               width : '100%',
+               padding : SIZES.font
+           }}>
+               <NFTitle title = {data.name} subTitle = {data.creator} titleSize= {SIZES.large} subTitleSize = {SIZES.small} />
+               <View style = {{
+                   marginTop : SIZES.font,
+                   flexDirection : 'row',
+                   justifyContent : 'space-between',
+                   alignItems : 'center'
+               }}>
+                   <ETHPrice price = {data.price} />
+                   <RectButton minWidth = {120} fontSize ={SIZES.font} handlePress = {() => navigation.navigate("Details" , {data})} />
+               </View>
+            </View> 
          
     </View>
   )
